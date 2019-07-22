5c900f21a79232008d11b7093294c05fb6872adf
joshfng <121190566+joshfng@users.noreply.github.com>
2019-07-21 20:34:40 -0400

    Better bitrate calculation

--- server/filters.go
+++ server/filters.go
@@ -9,7 +9,7 @@ import (
 
 // CalcBitrate holds information for a stream's inbound bitrate
 type CalcBitrate struct {
-	Bitrate float64
+	Bitrate int64
 
 	bitCnt   int64
 	lastTime int64
@@ -20,28 +20,29 @@ type CalcBitrate struct {
 
 // ModifyPacket accepts a filter to perform actions on the packet
 func (bitrate *CalcBitrate) ModifyPacket(pkt *av.Packet, streams []av.CodecData, videoidx int, audioidx int) (drop bool, err error) {
+	drop = false
+
+	// if pkt.Idx != int8(videoidx) { // || !pkt.IsKeyFrame {
+	// 	return
+	// }
+
 	// TODO: Pretty sure this is somewhat incorrect
 	bitrate.bitCnt += int64(len(pkt.Data))
 
-	if bitrate.lastTime+(1000000000) <= time.Now().UnixNano() {
-		bitrate.Bitrate = float64(bitrate.bitCnt / 500) // this should be 1000
+	// log kb/s ever 2 seconds
+	if bitrate.lastTime+(2000000000) <= time.Now().UnixNano() {
 		bitrate.lastTime = time.Now().UnixNano()
+
+		bitrate.Bitrate = bitrate.bitCnt
 
 		if bitrate.Channel != nil {
 			bitrate.Channel.RxBitrate = bitrate.Bitrate
 
-			log.Debugf("%s bitrate %.0f kb/s\n", bitrate.Channel.URL, bitrate.Channel.RxBitrate)
-		}
-
-		if bitrate.OutputStream != nil {
-			bitrate.OutputStream.TxBitrate = bitrate.Bitrate
-
-			log.Debugf("%s bitrate %.3f kb/s\n", bitrate.OutputStream.URL, bitrate.OutputStream.TxBitrate)
+			log.Debugf("%s bitrate %.0f kb/s", bitrate.Channel.URL, float64(bitrate.Channel.RxBitrate/2/1000))
 		}
 
 		bitrate.bitCnt = 0
 	}
 
-	drop = false
 	return
 }
